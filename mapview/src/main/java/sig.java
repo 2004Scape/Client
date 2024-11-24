c66849a970421a089e6b1dc3b70c0f4516785bff
Pazaz <pazaz@itspazaz.com>
2024-11-24 01:51:24 -0500

    fix: Placeholder mapview sig class

---
+++ mapview/src/main/java/sig.java
@@ -0,0 +1,5 @@
+public class sig  {
+	public static final int len = 325557;
+
+	public static final int[] sha = { -97, -86, -83, -84, -114, 88, -21, 25, 35, -40, 108, 11, 121, -27, -59, -40, -108, 126, -92, -23 };
+}
